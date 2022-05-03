d3ef14ff259abba95727e0026448e72657e931fa
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2022-05-03 18:03:02 -0300

    Feature(minimun-base)

--- next-base/src/hooks/useActions/useActions.ts
+++
@@ -1,6 +0,0 @@
-import { useMemo } from 'react'
-import { combineActions } from '../../redux/actionCreators'
-import store from '../../redux/store'
-
-const useActions = () => useMemo(() => combineActions(store.dispatch), [])
-export default useActions
--- next-base/src/redux/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import modalReducer from './modal'
-
-const reducers = combineReducers({
-  modal: modalReducer
-})
-
-export default reducers
