2f5544978c7e837c9c4219ba5f54b1a58895b64d
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2022-05-05 14:49:14 -0300

    Remove import

--- next-base/src/utils/index.ts
+++ next-base/src/utils/index.ts
@@ -1,3 +1,2 @@
 export { default as createActionCreator } from './createActionCreator'
 export { default as groupActionCreatorsSettingDispatch } from './groupActionCreatorsSettingDispatch'
-export { default as voidCall } from './void'
