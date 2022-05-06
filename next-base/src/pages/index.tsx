e3538694eaab03d0b9bb356988d6293421b91d24
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2022-05-06 11:05:00 -0300

    LoadingButton

--- next-base/src/pages/index.tsx
+++ next-base/src/pages/index.tsx
@@ -1,9 +1,26 @@
+import { Button, Stack } from '@mui/material'
+import { LoadingButton } from 'components/common'
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+
+  const [ loading, setLoading ] = useState(false)
+
   return (
     <div>
-      app
+      <Button
+        onClick={() => setLoading(v => !v)}
+      >
+        Toggle
+      </Button>
+      <br />
+      <LoadingButton
+        keepShape
+        loading={ loading }
+      >
+        long text
+      </LoadingButton>
     </div>
   )
 }
