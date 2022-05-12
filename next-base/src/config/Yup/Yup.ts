088a6ef580260b5a0abab801443f1a07977a8bcf
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2022-05-12 11:11:37 -0300

    Update Yup.ts

--- next-base/src/config/Yup/Yup.ts
+++ next-base/src/config/Yup/Yup.ts
@@ -1,30 +1,63 @@
 import * as yup from 'yup'
 
-yup.setLocale({
-  string: {
-    email: 'Email inválido',
-    min: 'Mínimo de ${min} caracteres',
-    max: 'Máximo de ${max} caracteres',
-    url: 'URL inválida',
-    trim: 'Espaços em branco não são permitidos',
-    lowercase: 'Somente letras minúsculas',
-    uppercase: 'Somente letras maiúsculas',
-    uuid: 'Id inválido',
-  },
-  mixed: {
-    default: 'Campo inválido',
-    required: 'Campo obrigatório',
-  },
-  date: {
-    max: 'Data maior que o limite',
-    min: 'Data menor que o limite',
-  },
-  number: {
-    max: 'Número maior que o limite',
-    min: 'Número menor que o limite',
-    negative: 'Número deve ser negativo',
-    positive: 'Número deve ser positivo',
-  }
-})
+type Lang = 'pt-br' | 'en-us'
 
-export default yup
+const create = (lang: Lang) => {
+  const dictionary: {[key in Lang]: ReturnType<typeof yup.setLocale>} = {
+    'pt-br': yup.setLocale({
+      string: {
+        email: 'Email inválido',
+        min: 'Mínimo de ${min} caracteres',
+        max: 'Máximo de ${max} caracteres',
+        url: 'URL inválida',
+        trim: 'Espaços em branco não são permitidos',
+        lowercase: 'Somente letras minúsculas',
+        uppercase: 'Somente letras maiúsculas',
+        uuid: 'Id inválido',
+      },
+      mixed: {
+        default: 'Campo inválido',
+        required: 'Campo obrigatório',
+      },
+      date: {
+        max: 'Data maior que o limite',
+        min: 'Data menor que o limite',
+      },
+      number: {
+        max: 'Número maior que o limite',
+        min: 'Número menor que o limite',
+        negative: 'Número deve ser negativo',
+        positive: 'Número deve ser positivo',
+      }
+    }),
+    'en-us': yup.setLocale({
+      string: {
+        email: 'Invalid email',
+        min: 'Minimum of ${min} characters',
+        max: 'Maximum of ${max} characters',
+        url: 'Invalid URL',
+        trim: 'Spaces are not allowed',
+        lowercase: 'Only lowercase letters',
+        uppercase: 'Only uppercase letters',
+        uuid: 'Invalid id',
+      },
+      mixed: {
+        default: 'Invalid field',
+        required: 'Required field',
+      },
+      date: {
+        max: 'Date is greater than the limit',
+        min: 'Date is less than the limit',
+      },
+      number: {
+        max: 'Number is greater than the limit',
+        min: 'Number is less than the limit',
+        negative: 'Number must be negative',
+        positive: 'Number must be positive',
+      }
+    })
+  }
+  return dictionary[lang]
+}
+
+export default create('pt-br')
